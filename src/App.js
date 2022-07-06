80a6cdf676050223df4d2014cbc0a9df78d8e5af
bilal <bilal@savahapp.com>
2022-07-06 11:11:35 +0100

    chartData

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   const [ open, setOpen ] = useState(false);
   const [ tableData, setTable ] = useState([]);
   const [ chartData, setChartData ] = useState([]);
-  const [ chartData2, setChartData2 ] = useState({});
 
   const columns = [
     {
@@ -39,19 +38,44 @@ function App() {
       const data = await res.json()
 
       let objects = data.near_earth_objects;
-
       let objectArr = [];
       for (const item in objects) {
         objectArr = objectArr.concat(objects[ item ]);
       }
       setTable(objectArr);
-      //console.log({ chartData });
+      //console.log({ objectArr });
+
+      let chartData = [];
+      objectArr.forEach(item => {
+        item.close_approach_data.forEach((item2, key) => {
+
+          chartData.push({
+            uv: 400,
+            pv: item2.miss_distance.astronomical, amt: 2400,
+            name: 'astronomical'
+          });
+          chartData.push({
+            uv: item2.miss_distance.kilometers,
+            pv: 2400, amt: 2400,
+            name: 'kilometers'
+          });
+          chartData.push({
+            uv: item2.miss_distance.lunar,
+            pv: 2400, amt: 2400,
+            name: 'lunar'
+          });
+          chartData.push({
+            uv: item2.miss_distance.miles,
+            pv: 2400, amt: 2400,
+            name: 'miles'
+          });
+
+        })
+        //console.log({ chartData });
+        setChartData(chartData);
+      });
+
     }
-
-
-
-
-
   }, [])
 
 
@@ -59,7 +83,7 @@ function App() {
     <div className="App">
       <h1>NasaAPI React-app</h1>
       <p>using Asteroids NeoWs (Near Earth Object Web Service)</p>
-      <p> Github link to this <a href="https://github.com/bilalma/" target="_blank">project</a></p>
+      <p> Github link to this <a href="https://github.com/bilalma/NASA-nearEarth-React-app" target="_blank">project</a></p>
       <div>
         <input className='input' placeholder='enter your key' type='text'></input>
         <button className='button' onClick={() => setOpen(true)}>SUBMIT</button>
@@ -67,12 +91,39 @@ function App() {
       </div>
       {open === true ?
         <div>
+          {/*  Table DATA    */}
           <div className='container'>
             <p style={{ color: 'white' }}><strong>Table showing Asteroids data with sort mechanism </strong></p>
             <DataTable
               columns={columns}
               data={tableData}
             />
+          </div>
+          {/*  Chart DATA    */}
+          <div className='container'>
+            {/* <BarChart
+              width={500}
+              height={300}
+              data={chartData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 80,
+                bottom: 5,
+              }}
+              barSize={20}
+            >
+              <XAxis
+                dataKey="name"
+                scale="point"
+                padding={{ left: 10, right: 10 }}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Bar dataKey="users" fill="#8884d8" background={{ fill: "#eee" }} />
+            </BarChart> */}
           </div>
         </div> :
         <h3 style={{ margin: '100px' }}>Enter your key to see Chart and Table data</h3>}
